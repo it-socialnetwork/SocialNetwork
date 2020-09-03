6240035a209572e0cad16e1e4bc3eb5532015ee1
Armada95 <nicolas.loyez9559@gmail.com>
2020-09-03 09:30:52 +0200

    suppression import inutile dans le back

--- vywatch/src/main/java/com/socialnetwork/vywatch/Repository/ListSubjectRepository.java
+++ vywatch/src/main/java/com/socialnetwork/vywatch/Repository/ListSubjectRepository.java
@@ -3,8 +3,7 @@ package com.socialnetwork.vywatch.Repository;
 import java.util.List;
 
 import com.socialnetwork.vywatch.Model.ListSubject;
-import com.socialnetwork.vywatch.Model.User;
-import com.socialnetwork.vywatch.Model.UserSubject;
+
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
