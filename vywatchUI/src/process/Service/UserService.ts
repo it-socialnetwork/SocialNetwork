07e69c84377065ee900347807a64da3bb76caed0
Armada95 <nicolas.loyez9559@gmail.com>
2020-09-05 12:01:22 +0200

    Modification de la méthode signin dans UserService

--- vywatchUI/src/process/Service/UserService.ts
+++ vywatchUI/src/process/Service/UserService.ts
@@ -1,6 +1,7 @@
 import{User} from '../Model/User'
 import { HttpClient, HttpRequest, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 const httpOptions = {
     headers: new HttpHeaders({
@@ -11,7 +12,7 @@ const httpOptions = {
 export class UserService{
 
   user:User
-    constructor(private readonly http : HttpClient){}
+    constructor(private readonly http : HttpClient, private _router: Router){}
 
     displaySubject() {
       return this.http.get("http://localhost:8080/vywatch/api/user/getalluser",httpOptions)
@@ -29,19 +30,32 @@ export class UserService{
         ) ;
     }
 
-    signInUser (username:String, password:String){
-          this.user = new User()
+     signInUser (username:String, password:String, inscription:boolean){
+        this.user = new User()
         this.user.pseudo = username
         this.user.password = password
-        return this.http.post("http://localhost:8080/vywatch/api/authenticate", this.user, {responseType : 'text' as 'json'}).subscribe(res => {
-          console.log("User service : on récupere la reponse de la requete : normalement un token")
-          console.log(res);
+        return this.http.post("http://localhost:8080/vywatch/api/authenticate", this.user, {responseType : 'text' as 'json'}).subscribe(async res => {
+          localStorage.setItem('pseudo', String(username));
           localStorage.setItem('token', String(res));
+          if(inscription==false)
+          {
+            this._router.navigate(['pageMembre/',  localStorage.getItem('pseudo')], {queryParams: {}});
+          }
+          else
+          {
+            await this.delay(2000)
+            this._router.navigate(['preference/', localStorage.getItem('pseudo')], {queryParams: {}});
+          }
         },
         (error) => {
           console.log('Erreur ! : ' + error);
         }
         );
+    }
+
+    private delay(ms: number)
+    {
+      return new Promise(resolve => setTimeout(resolve, ms));
     }
 
 
