2c0a4cb216685dc59680634e13e13fe478e9fa22
Armada95 <nicolas.loyez9559@gmail.com>
2020-09-08 20:34:57 +0200

    ajout animation sur page main

--- vywatchUI/src/app/main/main.component.ts
+++ vywatchUI/src/app/main/main.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+
+import{faHandshake} from '@fortawesome/free-solid-svg-icons'
+import{faPrayingHands} from '@fortawesome/free-solid-svg-icons'
+import{faAtom} from '@fortawesome/free-solid-svg-icons'
+import{faMonument} from '@fortawesome/free-solid-svg-icons'
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+
+  faHandshake=faHandshake
+  faPrayingHands=faPrayingHands
+  faAtom=faAtom
+  faMonument=faMonument
 
   selectedFile: File
   constructor() { }
