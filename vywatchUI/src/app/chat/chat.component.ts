8fd788afc40e70700d35a2c6474d070025ef22f0
Armada95 <nicolas.loyez9559@gmail.com>
2020-09-19 14:48:38 +0200

    messagerie côté front pour test mise en place

--- vywatchUI/src/app/chat/chat.component.ts
+++ vywatchUI/src/app/chat/chat.component.ts
@@ -7,7 +7,7 @@ import { ChatService } from '../chat.service';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
-  message: string;
+  /*message: string;
   messages: string[] = [];
 
   constructor(private chatService: ChatService) {
@@ -22,5 +22,39 @@ export class ChatComponent implements OnInit {
     this.chatService.getMessages().subscribe((message: string) => {
         this.messages.push(message);
       });
+  }*/
+
+  ngOnInit() {
+   
   }
+
+  user:String;
+    room:String;
+    messageText:String;
+    messageArray:Array<{user:String,message:String}> = [];
+    constructor(private _chatService:ChatService){
+        this._chatService.newUserJoined()
+        .subscribe(data=> this.messageArray.push(data));
+
+
+        this._chatService.userLeftRoom()
+        .subscribe(data=>this.messageArray.push(data));
+
+        this._chatService.newMessageReceived()
+        .subscribe(data=>this.messageArray.push(data));
+    }
+
+    join(){
+        this._chatService.joinRoom({user:this.user, room:this.room});
+    }
+
+    leave(){
+        this._chatService.leaveRoom({user:this.user, room:this.room});
+    }
+
+    sendMessage()
+    {
+        this._chatService.sendMessage({user:this.user, room:this.room, message:this.messageText});
+    }
+
 }
