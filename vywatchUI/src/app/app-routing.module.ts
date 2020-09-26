65ee7b5db1324d67dafdb6f7e761787529a7471f
Armada95 <nicolas.loyez9559@gmail.com>
2020-09-26 10:23:05 +0200

    Correction problème avec composant supprimer

--- vywatchUI/src/app/app-routing.module.ts
+++ vywatchUI/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {PageloginComponent} from './pagelogin/pagelogin.component';
+
 import {CountSubscribeComponent} from './count-subscribe/count-subscribe.component';
 import {SubjectUserChoiceComponent} from './subject-user-choice/subject-user-choice.component'
 import {MainComponent} from './main/main.component'
